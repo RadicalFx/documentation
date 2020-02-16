2e0cd270832fa7c1b1e4e1f8d8f16bd6684a1a52
Mauro Servienti <mauro.servienti@gmail.com>
2020-02-16 19:48:50 +0100

    new application boot style

--- samples/Showcases/Radical.Samples/App.xaml.cs
+++ samples/Showcases/Radical.Samples/App.xaml.cs
@@ -9,23 +9,25 @@ namespace Radical.Samples
     {
         public App()
         {
-            var bootstrapper = new ApplicationBootstrapper<Presentation.MainView>()
-                .EnableSplashScreen()
-                .OnBoot(container =>
+            this.AddRadicalApplication<Presentation.MainView>(configuration =>
+            {
+                configuration.EnableSplashScreen();
+                configuration.OnBooting(container => 
                 {
-                   var conventions = container.GetService<IConventionsHandler>();
-                   /*
-                    * the following will make every ViewModel 
-                    * appear as a resource in bound Views
-                    * --> conventions.ShouldExposeViewModelAsStaticResource = (view, viewModel) => true;
-                    */
-                   conventions.ShouldExposeViewModelAsStaticResource = (view, viewModel) =>
-                   {
-                       return (viewModel.GetType() == typeof(SampleViewModelAsResourceViewModel))
-                        ? true
-                        : conventions.DefaultShouldExposeViewModelAsStaticResource(view, viewModel);
-                   };
+                    var conventions = container.GetService<IConventionsHandler>();
+                    /*
+                     * the following will make every ViewModel 
+                     * appear as a resource in bound Views
+                     * --> conventions.ShouldExposeViewModelAsStaticResource = (view, viewModel) => true;
+                     */
+                    conventions.ShouldExposeViewModelAsStaticResource = (view, viewModel) =>
+                    {
+                        return (viewModel.GetType() == typeof(SampleViewModelAsResourceViewModel))
+                         ? true
+                         : conventions.DefaultShouldExposeViewModelAsStaticResource(view, viewModel);
+                    };
                 });
+            });
         }
     }
 }
