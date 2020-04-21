7f5d993688b8af26deedb04f2e50763e2f3f48c1
umberto <ucollina82@gmail.com>
2020-04-21 17:18:46 +0200

    - new solution for autocomplete example - completed example

---
+++ samples/AutocompleteSample/AutocompleteSample/Providers/PersonSuggestionProvider.cs
@@ -0,0 +1,25 @@
+﻿using AutocompleteSample.BindingObjects;
+using AutoCompleteTextBox.Editors;
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace AutocompleteSample.Providers
+{
+    public class PersonSuggestionProvider : ISuggestionProvider
+    {
+        public IEnumerable GetSuggestions(string filter)
+        {
+            return filter.Length >= 3 ? storage.Where(p => p.FullName.StartsWith(filter, StringComparison.OrdinalIgnoreCase)) : default;
+        }
+
+        Person[] storage = new []
+        {
+            new Person(){ FirstName = "Mauro", LastName = "Servienti" },
+            new Person(){ FirstName = "Giorgio", LastName = "Formica" },
+            new Person(){ FirstName = "Giorgio", LastName = "Gentili" },
+            new Person(){ FirstName = "Giorgio", LastName = "Gerosa" },
+            new Person(){ FirstName = "Daniele", LastName = "Restelli" }
+        };
+    }
+}
