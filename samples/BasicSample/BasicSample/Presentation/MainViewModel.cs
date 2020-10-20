b326a9ab52f11507647c98ba3866a338996705b0
mauroservienti <mauro.servienti@gmail.com>
2020-10-20 13:07:24 +0200

    Simple bound property

--- samples/BasicSample/BasicSample/Presentation/MainViewModel.cs
+++ samples/BasicSample/BasicSample/Presentation/MainViewModel.cs
@@ -3,7 +3,17 @@
 namespace BasicSample.Presentation
 {
     public class MainViewModel : AbstractViewModel
-    {    
-        
+    {
+        public MainViewModel()
+        {
+            var propertyMetadata = GetPropertyMetadata(() => Text);
+            propertyMetadata.DefaultValue = "Text property default value";
+        }
+
+        public string Text
+        {
+            get => GetPropertyValue(()=>Text);
+            set => SetPropertyValue(()=> Text, value);
+        }
     }
 }
