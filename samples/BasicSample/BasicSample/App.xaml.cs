b215a3c07e6a59c6d4b6dbb663a7d3e76fd8bf53
Mauro Servienti <mauro.servienti@gmail.com>
2020-10-20 13:09:07 +0200

    Add a basic empty sample (#164)
    * Add a basic empty sample
    
    * not needed reference
    
    * not needed XML namespaces
    
    * Simple bound property

---
+++ samples/BasicSample/BasicSample/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace BasicSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.AddRadicalApplication<Presentation.MainView>();
+        }
+    }
+}
---
+++ samples/BasicSample/BasicSample/Presentation/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Radical.Windows.Presentation;
+
+namespace BasicSample.Presentation
+{
+    public class MainViewModel : AbstractViewModel
+    {
+        public MainViewModel()
+        {
+            var propertyMetadata = GetPropertyMetadata(() => Text);
+            propertyMetadata.DefaultValue = "Text property default value";
+        }
+
+        public string Text
+        {
+            get => GetPropertyValue(()=>Text);
+            set => SetPropertyValue(()=> Text, value);
+        }
+    }
+}
